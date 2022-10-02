cdb68fe0de27d892b66caceeab127bbd17321179
Ulofun <rakovod1337@gmail.com>
2022-10-03 00:07:11 +0300

    Task_4

---
+++ TestGB_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Возвести число А в степень В
+
+Console.WriteLine("Введите первое число ");
+double number_A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+double number_B = Convert.ToDouble(Console.ReadLine());
+double result = find_number(number_A, number_B);
+
+Console.WriteLine("Результат вычисления равен = " + result);
+
+
+double find_number( double number_A, double number_B)
+{ 
+    double find_num = Math.Pow (number_A, number_B);
+    double result = find_num;
+    return result;
+}
